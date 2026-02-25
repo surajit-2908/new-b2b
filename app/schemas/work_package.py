f8d985300c5f8958b42303e0cc76709f9f3b38ac
ishaansg <anindya.sengupta2021@gmail.com>
2026-02-25 14:26:52 +0530

    updated field name

--- app/schemas/work_package.py
+++ app/schemas/work_package.py
@@ -116,7 +116,7 @@ class AdminPackageOut(BaseModel):
     required_skills: List[SkillsOut]
     lowest_bid: Optional[float] = None
     lead_id: int
-    technician: Optional[UserOut] = None
+    assigned_technician: Optional[UserOut] = None
       
     model_config = ConfigDict(
         from_attributes=True,
